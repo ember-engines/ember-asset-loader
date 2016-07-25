65bc6ffcea9761d9d8d8ac6dd39725aea2cb2650
Trent Willis <twillis@linkedin.com>
2016-07-25 16:45:11 -0700

    Generate asset manifest during postprocessTree

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-asset-loader'
+  name: 'ember-asset-loader',
+
+  postprocessTree: function(type, tree) {
+    if (type === 'all') {
+      tree = require('./lib/generate-asset-manifest')(tree); // eslint-disable-line global-require
+    }
+
+    return tree;
+  }
 };

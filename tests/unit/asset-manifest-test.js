e5bbdebd3df5d4d59c76ee63a0180a91e2db1d4c
Robert Jackson <rjackson@linkedin.com>
2017-08-08 15:12:18 -0400

    Refactor from `require` to `requirejs` usage.
    Babel 6 has _many_ issues when using the `require` global directly.
    
    This avoids the issue by moving to using `requirejs` instead.

--- tests/unit/asset-manifest-test.js
+++ tests/unit/asset-manifest-test.js
@@ -1,27 +1,28 @@
-/* global require */
+/* global requirejs, define */
 
 import { module, test } from 'qunit';
+import require from 'require';
 
 module('Unit | asset-manifest', {
   beforeEach() {
     resetModules();
-    this.originalNodeModule = require.entries['dummy/config/node-asset-manifest'];
+    this.originalNodeModule = requirejs.entries['dummy/config/node-asset-manifest'];
   },
 
   afterEach() {
-    require.entries['dummy/config/node-asset-manifest'] = this.originalNodeModule;
+    requirejs.entries['dummy/config/node-asset-manifest'] = this.originalNodeModule;
     resetModules();
   }
 });
 
 function resetModules() {
-  require.unsee('dummy/config/node-asset-manifest');
-  require.unsee('dummy/config/asset-manifest');
+  requirejs.unsee('dummy/config/node-asset-manifest');
+  requirejs.unsee('dummy/config/asset-manifest');
 }
 
 test('node-asset-manifest is generated properly', function(assert) {
   const nodeManifest = require('dummy/config/node-asset-manifest').default;
-  delete require.entries['dummy/config/node-asset-manifest'];
+  delete requirejs.entries['dummy/config/node-asset-manifest'];
 
   const manifest = require('dummy/config/asset-manifest').default;
 
@@ -37,7 +38,7 @@ test('loads the node-asset-manifest if present', function(assert) {
 });
 
 test('loads the manifest from the meta tag if available', function(assert) {
-  delete require.entries['dummy/config/node-asset-manifest'];
+  delete requirejs.entries['dummy/config/node-asset-manifest'];
 
   const meta = document.querySelector('meta[name="dummy/config/asset-manifest"]');
   const metaContent = meta.getAttribute('content');
@@ -47,7 +48,7 @@ test('loads the manifest from the meta tag if available', function(assert) {
 });
 
 test('throws an error if unable to load the manifest', function(assert) {
-  delete require.entries['dummy/config/node-asset-manifest'];
+  delete requirejs.entries['dummy/config/node-asset-manifest'];
 
   const meta = document.querySelector('meta[name="dummy/config/asset-manifest"]');
   const metaContent = meta.getAttribute('content');

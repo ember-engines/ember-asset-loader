93924e00a255adc1c87fc4edaebae0842202f8b2
Evan Farina <farina.evan@gmail.com>
2020-01-08 13:55:15 -0500

    Transform tests to 'setup-test' syntax

--- app/config/asset-manifest.js
+++ app/config/asset-manifest.js
@@ -6,24 +6,19 @@ const metaName = `${modulePrefix}/config/asset-manifest`;
 const nodeName = `${modulePrefix}/config/node-asset-manifest`;
 
 let config = {};
-const lookupManifest = !environment.noManifestLookup;
 
-if (lookupManifest) {
-  try {
-    // If we have a Node version of the asset manifest, use that for FastBoot and
-    // similar environments.
-    if (require.has(nodeName)) {
-      config = require(nodeName).default; // eslint-disable-line
-    } else {
-      const rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
-      config = JSON.parse(unescape(rawConfig));
-    }
-  } catch(err) {
-    throw new Error('No manifest was found with the name "' + metaName +
-      '". To hide this message you can set the config option `noManifestLookup` to `true`. For non-browser environments, verify that you included the node-asset-manifest module.', {
-        id: 'no-manifest-found'
-      });
+try {
+  // If we have a Node version of the asset manifest, use that for FastBoot and
+  // similar environments.
+  if (require.has(nodeName)) {
+    config = require(nodeName).default; // eslint-disable-line
+  } else {
+    const rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
+    config = JSON.parse(unescape(rawConfig));
   }
+} catch(err) {
+  throw new Error('Failed to load asset manifest. For browser environments, verify the meta tag with name "'+ metaName +
+    '" is present. For non-browser environments, verify that you included the node-asset-manifest module.');
 }
 
 export default config;

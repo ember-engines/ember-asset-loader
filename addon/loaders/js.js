daf624b08f4fa8c97b9a9a486bb930ed0bd8bf6b
Michael Villander <nousvillander@gmail.com>
2020-05-10 19:57:00 -0300

    Add the script tag after-render

--- addon/loaders/js.js
+++ addon/loaders/js.js
@@ -1,5 +1,6 @@
 import RSVP from 'rsvp';
 import { createLoadElement, nodeLoader } from './utilities';
+import { scheduleWork } from './scheduler';
 
 /**
  * Default loader function for JS assets. Loads them by inserting a script tag
@@ -15,16 +16,29 @@ export default nodeLoader(function js(uri) {
       return resolve();
     }
 
-    const script = createLoadElement('script', resolve, function(error) {
-      if (this.parentNode) {
-        this.parentNode.removeChild(this);
+    // DOM mutation should be batched, this indirection enables this batching.
+    // By default, it will schedule work on the next afterRender queue. But can
+    // be configured for further control via.
+    //
+    // ```js
+    //  import { setScheduler } from 'ember-asset-loader/scheduler';
+    //
+    //  setScheduler(function(work /* work is a callback */) {
+    //    someScheduler.scheduleWork(work);
+    //  });
+    // ```
+    //
+    scheduleWork(() => {
+      try {
+        const script = createLoadElement('script', resolve, reject);
+
+        script.src = uri;
+        script.async = false;
+
+        document.head.appendChild(script);
+      } catch(e) {
+        reject(e);
       }
-      reject(error);
     });
-
-    script.src = uri;
-    script.async = false;
-
-    document.head.appendChild(script);
   });
 });
